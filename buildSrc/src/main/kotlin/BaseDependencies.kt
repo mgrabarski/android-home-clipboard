2462285602d75e79b8387ba905b8601c9c9a694b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-28 11:31:22 +0200

    Added base dependencies

--- buildSrc/src/main/kotlin/BaseDependencies.kt
+++ buildSrc/src/main/kotlin/BaseDependencies.kt
@@ -6,3 +6,15 @@ object BaseDependencies {
 object Dependencies {
     val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
 }
+
+object AndroidDependencies {
+    val core = "androidx.core:core-ktx:${Versions.androidCore}"
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val material = "com.google.android.material:material:${Versions.material}"
+    val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+}
+
+object UnitTestDependencies {
+    val junit = "org.junit.jupiter:junit-jupiter-api:${Versions.junit}"
+    val junitJupiterEngine = "org.junit.jupiter:junit-jupiter-engine"
+}
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,4 +1,9 @@
 object Versions {
     val gradle = "4.2.1"
     val kotlin = "1.5.0"
+    val androidCore = "1.5.0"
+    val appCompat = "1.3.0"
+    val material = "1.3.0"
+    val constraintLayout = "2.0.4"
+    val junit = "5.7.1"
 }
