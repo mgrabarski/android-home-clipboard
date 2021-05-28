f166eca51478e7a44442dbe0ee76a684a8a9e4dd
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-28 11:18:14 +0200

    Added buildSrc with application base config

---
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -0,0 +1,19 @@
+object ApplicationConfig {
+
+    val compileSdkVersion = 30
+    val minSdkVersion = 23
+    val targetSdkVersion = 30
+    val versionCode = 1
+    val versionName = "0.0.1"
+}
+
+object Flavours {
+
+    val productionApplicationId = "com.mg.homeclipboards"
+    val stageApplicationId = "com.mg.homeclipboards.stage"
+}
+
+object Java {
+
+    val java_version = "1.8"
+}
