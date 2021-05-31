7685e1a72d2f679b88fb6dd780691c9a6b9cc059
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-05-31 10:39:53 +0200

    Added resources module

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -2,6 +2,6 @@ object Modules {
     val appConstants = ":constants"
     val domain = ":domain"
     val libComponents = ":libraries:components"
-
+    val resources = ":shared:resources"
     val featureAddNewClipboard = ":features:add-clipboard"
 }
