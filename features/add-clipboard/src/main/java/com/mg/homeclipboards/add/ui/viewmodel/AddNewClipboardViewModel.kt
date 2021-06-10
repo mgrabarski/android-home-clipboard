28944e101e502d5b12981b884fdacdeb39e30fa9
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-10 07:30:52 +0200

    Handle create new clipboard use case

--- features/add-clipboard/src/main/java/com/mg/homeclipboards/add/ui/viewmodel/AddNewClipboardViewModel.kt
+++ features/add-clipboard/src/main/java/com/mg/homeclipboards/add/ui/viewmodel/AddNewClipboardViewModel.kt
@@ -6,8 +6,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mg.homeclipboards.domain.interactor.clipboard.CreateNewClipboard
+import com.mg.homeclipboards.domain.interactor.clipboard.ERROR_CLIPBOARD_NAME_IS_EMPTY
+import com.mg.homeclipboards.domain.interactor.clipboard.ERROR_SAVE_CLIPBOARD_NOT_PASS
 import com.mg.homeclipboards.domain.model.Icon
 import com.mg.homeclipboards.domain.model.Icon.Fridge
+import com.mg.homeclipboards.domain.state.Failure
+import com.mg.homeclipboards.domain.state.Success
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -32,7 +36,32 @@ class AddNewClipboardViewModel(
     }
 
     fun saveClipboard() {
+        viewModelScope.launch {
+            createNewClipboard.create(
+                name = clipboardName.get().orEmpty(),
+                icon = _selectedIcon.value!!.icon
+            ).collect {
+                when (it) {
+                    is Success -> handleSuccessSaveClipboard()
+                    is Failure -> handleFailureSaveClipboard(it)
+                }
+            }
+        }
+    }
 
+    private fun handleSuccessSaveClipboard() {
+
+    }
+
+    private fun handleFailureSaveClipboard(useCaseResult: Failure) {
+        when (useCaseResult.message) {
+            ERROR_CLIPBOARD_NAME_IS_EMPTY -> {
+
+            }
+            ERROR_SAVE_CLIPBOARD_NOT_PASS -> {
+
+            }
+        }
     }
 
     data class SelectedIcon(
