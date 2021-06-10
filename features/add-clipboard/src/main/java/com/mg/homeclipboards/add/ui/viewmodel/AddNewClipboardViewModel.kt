d936b11b994cc7e380730ca85a9efd2dc244c9e1
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-10 07:14:24 +0200

    Added clipboard name observable field

--- features/add-clipboard/src/main/java/com/mg/homeclipboards/add/ui/viewmodel/AddNewClipboardViewModel.kt
+++ features/add-clipboard/src/main/java/com/mg/homeclipboards/add/ui/viewmodel/AddNewClipboardViewModel.kt
@@ -1,16 +1,27 @@
 package com.mg.homeclipboards.add.ui.viewmodel
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mg.homeclipboards.domain.interactor.clipboard.CreateNewClipboard
 import com.mg.homeclipboards.domain.model.Icon
 import com.mg.homeclipboards.domain.model.Icon.Fridge
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class AddNewClipboardViewModel : ViewModel() {
+class AddNewClipboardViewModel(
+    private val createNewClipboard: CreateNewClipboard
+) : ViewModel() {
 
     private val _selectedIcon = MutableLiveData<SelectedIcon>()
     val selectedIcon: LiveData<SelectedIcon>
         get() = _selectedIcon
+
+    var clipboardName = ObservableField<String>().apply {
+        set("")
+    }
 
     init {
         _selectedIcon.value = SelectedIcon(Fridge)
@@ -18,6 +29,10 @@ class AddNewClipboardViewModel : ViewModel() {
 
     fun onSelectIconChange(icon: SelectedIcon) {
         _selectedIcon.value = icon
+    }
+
+    fun saveClipboard() {
+
     }
 
     data class SelectedIcon(
