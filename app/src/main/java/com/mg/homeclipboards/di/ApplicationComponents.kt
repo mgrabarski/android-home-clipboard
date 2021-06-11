4dae6f54e3eb50d82ea6468fb02670ce4bdf1684
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-11 13:51:24 +0200

    Repository module di

--- app/src/main/java/com/mg/homeclipboards/di/ApplicationComponents.kt
+++ app/src/main/java/com/mg/homeclipboards/di/ApplicationComponents.kt
@@ -4,10 +4,12 @@ import com.mg.homeclipboard.database.di.databaseModule
 import com.mg.homeclipboards.add.di.addNewClipboardModule
 import com.mg.homeclipboards.components.di.libComponentsModule
 import com.mg.homeclipboards.domain.di.domainModule
+import com.mg.homeclipboards.repository.di.repositoryModule
 
 val appModules = listOf(
     libComponentsModule,
     domainModule,
     addNewClipboardModule,
-    databaseModule
+    databaseModule,
+    repositoryModule
 )
---
+++ core/repository/src/main/java/com/mg/homeclipboards/repository/di/RepositoryComponents.kt
@@ -0,0 +1,13 @@
+package com.mg.homeclipboards.repository.di
+
+import com.mg.homeclipboards.domain.repository.ClipboardRepository
+import com.mg.homeclipboards.repository.domainimpl.HomeClipboardRepository
+import com.mg.homeclipboards.repository.local.ClipboardLocalStorage
+import com.mg.homeclipboards.repository.remote.ClipboardRemoteStorage
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    factory { ClipboardLocalStorage(get()) }
+    factory { ClipboardRemoteStorage() }
+    factory<ClipboardRepository> { HomeClipboardRepository(get(), get()) }
+}
