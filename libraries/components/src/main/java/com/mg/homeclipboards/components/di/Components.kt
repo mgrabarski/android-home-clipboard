06aea950110d2a854b50223dd94e7572cb5fe4f4
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-11 10:39:41 +0200

    Rename component

--- libraries/components/src/main/java/com/mg/homeclipboards/components/di/Components.kt
+++ libraries/components/src/main/java/com/mg/homeclipboards/components/di/Components.kt
@@ -1,6 +1,6 @@
 package com.mg.homeclipboards.components.di
 
-import com.mg.homeclipboards.components.dispatchers.DefaultDispathersProvider
+import com.mg.homeclipboards.components.dispatchers.DefaultDispatcherProvider
 import com.mg.homeclipboards.components.dispatchers.DispatcherProvider
 import com.mg.homeclipboards.components.time.DateFormatter
 import com.mg.homeclipboards.components.time.TimeProvider
@@ -13,5 +13,5 @@ import org.koin.dsl.module
 val libComponentsModule = module {
     factory<DateFormatter> { DateFormatterJodaTime(FORMAT_DETAILED_DATE, FORMAT_SHORTER_DATE) }
     factory<TimeProvider> { TimeProviderImpl(get()) }
-    factory<DispatcherProvider> { DefaultDispathersProvider() }
+    factory<DispatcherProvider> { DefaultDispatcherProvider() }
 }
