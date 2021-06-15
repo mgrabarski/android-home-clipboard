89c42dad041a896d39fb113db1d75d52a4fafcfa
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-15 09:26:13 +0200

    Emit failure

--- domain/src/main/java/com/mg/homeclipboards/domain/interactor/user/CreateAndLoginNewUser.kt
+++ domain/src/main/java/com/mg/homeclipboards/domain/interactor/user/CreateAndLoginNewUser.kt
@@ -3,16 +3,26 @@ package com.mg.homeclipboards.domain.interactor.user
 import com.mg.homeclipboards.domain.model.User
 import com.mg.homeclipboards.domain.model.types.Id
 import com.mg.homeclipboards.domain.repository.UserRepository
+import com.mg.homeclipboards.domain.state.Failure
+import com.mg.homeclipboards.domain.state.UseCaseResult
+import kotlinx.coroutines.flow.flow
+
+const val ERROR_INSERT_NEW_USER = "Problem with insert new user"
 
 class CreateAndLoginNewUser(
     private val repository: UserRepository
 ) {
 
-    suspend fun create() {
+    suspend fun create() = flow<UseCaseResult<User>> {
         val user = User(id = Id.randomUUID())
 
         val numberOfInserts = repository.insertUser(user)
 
-        LoginUserHolder.loginUser = user
+        if (numberOfInserts > 0) {
+            LoginUserHolder.loginUser = user
+
+        } else {
+            emit(Failure(ERROR_INSERT_NEW_USER))
+        }
     }
 }
--- domain/src/test/kotlin/com/mg/homeclipboards/domain/interactor/user/CreateAndLoginNewUserTest.kt
+++ domain/src/test/kotlin/com/mg/homeclipboards/domain/interactor/user/CreateAndLoginNewUserTest.kt
@@ -1,10 +1,13 @@
 package com.mg.homeclipboards.domain.interactor.user
 
 import com.mg.homeclipboards.domain.repository.UserRepository
+import com.mg.homeclipboards.domain.state.Failure
 import com.mg.homeclipboards.utils.testBlocking
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.flow.collect
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
@@ -26,8 +29,17 @@ internal class CreateAndLoginNewUserTest {
     internal fun `Creates user in repository`() = testBlocking {
         coEvery { repository.insertUser(any()) } returns 1
 
-        sut.create()
+        sut.create().collect()
 
         coVerify(exactly = 1) { repository.insertUser(any()) }
+    }
+
+    @Test
+    internal fun `Create user emit failure when repository return 0 inserts`() = testBlocking {
+        coEvery { repository.insertUser(any()) } returns 1
+
+        sut.create().collect { result ->
+            result.shouldBeSameInstanceAs(Failure::class)
+        }
     }
 }
