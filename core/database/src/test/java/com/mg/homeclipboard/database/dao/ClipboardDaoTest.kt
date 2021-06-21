96ca39934e3f66b842d389dccabd7b045ce70e69
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-21 11:39:28 +0200

    Update for clipboard tests using user relation

--- core/database/src/test/java/com/mg/homeclipboard/database/dao/ClipboardDaoTest.kt
+++ core/database/src/test/java/com/mg/homeclipboard/database/dao/ClipboardDaoTest.kt
@@ -4,11 +4,12 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mg.clipboards.test.coroutine.testBlocking
 import com.mg.homeclipboard.database.DatabaseTestCase
 import com.mg.homeclipboard.database.dataset.anyHomeClipboardEntity
+import com.mg.homeclipboard.database.dataset.anyUser
+import com.mg.homeclipboard.database.entities.ClipboardEntity
+import com.mg.homeclipboard.database.entities.UserEntity
 import io.kotest.matchers.equality.shouldBeEqualToUsingFields
 import io.kotest.matchers.longs.shouldBeGreaterThan
 import io.kotest.matchers.nulls.shouldNotBeNull
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,27 +18,36 @@ import org.junit.runner.RunWith
 internal class ClipboardDaoTest : DatabaseTestCase() {
 
     private lateinit var dao: ClipboardDao
+    private lateinit var user: UserEntity
 
     @Before
-    override fun setUp() {
+    override fun setUp() = testBlocking {
         super.setUp()
         dao = database.clipboardDao()
+
+        user = anyUser()
+        database.userDao().insert(user)
     }
 
     @Test
     fun `Insert and read clipboard`() = testBlocking {
-        val clipboard = anyHomeClipboardEntity()
+        val clipboard = anyHomeClipboardEntity(userId = user.id)
         dao.insert(clipboard)
 
         val result = dao.findById(clipboard.id)
 
         result.shouldNotBeNull()
-        result.shouldBeEqualToUsingFields(clipboard)
+        result.shouldBeEqualToUsingFields(
+            clipboard,
+            ClipboardEntity::id,
+            ClipboardEntity::userId,
+            ClipboardEntity::name
+        )
     }
 
     @Test
     fun `Inserts return row id`() = testBlocking {
-        val clipboard = anyHomeClipboardEntity()
+        val clipboard = anyHomeClipboardEntity(userId = user.id)
 
         val rowId = dao.insert(clipboard)
 
--- core/database/src/test/java/com/mg/homeclipboard/database/dataset/Clipboard.kt
+++ core/database/src/test/java/com/mg/homeclipboard/database/dataset/Clipboard.kt
@@ -5,9 +5,10 @@ import com.mg.homeclipboards.domain.model.Icon
 import com.mg.homeclipboards.domain.model.types.Id
 import org.joda.time.DateTime
 
-fun anyHomeClipboardEntity() = ClipboardEntity(
+fun anyHomeClipboardEntity(userId: String = Id.randomUUID().toString()) = ClipboardEntity(
     id = Id.randomUUID().toString(),
     name = "name",
     icon = Icon.Fridge.name,
-    createDate = DateTime.now()
+    createDate = DateTime.now(),
+    userId = userId
 )
