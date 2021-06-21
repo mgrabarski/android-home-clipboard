73c5c489a9aec55bb310714604d03897cdd1ccfe
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-21 10:59:24 +0200

    Changed base dao to interface

--- core/database/src/main/java/com/mg/homeclipboard/database/dao/base/BaseDao.kt
+++ core/database/src/main/java/com/mg/homeclipboard/database/dao/base/BaseDao.kt
@@ -3,14 +3,14 @@ package com.mg.homeclipboard.database.dao.base
 import androidx.room.Insert
 import androidx.room.Update
 
-abstract class BaseDao<T> {
+interface BaseDao<T> {
 
     @Insert
-    abstract suspend fun insert(obj: T): Long
+    suspend fun insert(obj: T): Long
 
     @Insert
-    abstract suspend fun insert(objc: List<T>)
+    suspend fun insert(objc: List<T>)
 
     @Update
-    abstract suspend fun update(obj: T)
+    suspend fun update(obj: T)
 }
