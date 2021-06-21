6670dcc35cfe1188d8909a05b2ddae18d9b4ed5b
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-21 11:40:12 +0200

    Const update

--- constants/src/main/java/com/mg/homeclipboards/constants/Database.kt
+++ constants/src/main/java/com/mg/homeclipboards/constants/Database.kt
@@ -5,11 +5,17 @@ const val DATABASE_NAME = "HomeClipboardsDB.db"
 
 object Entities {
 
+    object User {
+        const val TABLE_USER = "user"
+        const val TABLE_USER_ID = "id"
+    }
+
     object Clipboard {
         const val TABLE_CLIPBOARD = "clipboard"
         const val TABLE_CLIPBOARD_ID = "id"
         const val TABLE_CLIPBOARD_NAME = "name"
         const val TABLE_CLIPBOARD_ICON = "icon"
         const val TABLE_CLIPBOARD_CREATE_DATE = "create_date"
+        const val TABLE_CLIPBOARD_USER_ID = "userId"
     }
 }
