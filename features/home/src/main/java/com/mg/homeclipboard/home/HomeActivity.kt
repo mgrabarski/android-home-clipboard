d1072632979f54f9ee6c2dd630b403f79fdc29de
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-25 13:33:34 +0200

    Added menu to home activity

--- features/home/src/main/java/com/mg/homeclipboard/home/HomeActivity.kt
+++ features/home/src/main/java/com/mg/homeclipboard/home/HomeActivity.kt
@@ -1,11 +1,30 @@
 package com.mg.homeclipboard.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.mg.homeclipboard.home.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.setting -> {
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
