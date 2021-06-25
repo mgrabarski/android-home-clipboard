738d5728171a2f3adf1f28fce26a16d78d765c20
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-06-25 14:21:50 +0200

    Added use case to DI

--- domain/src/main/java/com/mg/homeclipboards/domain/di/DomainComponents.kt
+++ domain/src/main/java/com/mg/homeclipboards/domain/di/DomainComponents.kt
@@ -4,6 +4,7 @@ import com.mg.homeclipboards.domain.data.factories.ClipboardFactory
 import com.mg.homeclipboards.domain.data.providers.LoginUserProvider
 import com.mg.homeclipboards.domain.data.providers.impl.LoginUserProviderImpl
 import com.mg.homeclipboards.domain.interactor.clipboard.CreateNewClipboard
+import com.mg.homeclipboards.domain.interactor.clipboard.LoadUserClipboards
 import com.mg.homeclipboards.domain.interactor.user.CreateAndLoginNewUser
 import com.mg.homeclipboards.domain.interactor.user.LoadLoginUser
 import kotlinx.coroutines.Dispatchers
@@ -15,4 +16,5 @@ val domainModule = module {
     factory { CreateAndLoginNewUser(get(), get()) }
     factory { LoadLoginUser(Dispatchers.IO, get(), get()) }
     factory<LoginUserProvider> { LoginUserProviderImpl() }
+    factory { LoadUserClipboards(get()) }
 }
