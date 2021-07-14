b77724a1389bdccc202804aed09c5f36ca5ec0d1
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-07-14 20:36:51 +0200

    Added tests for clipboard dao

--- core/database/src/test/java/com/mg/homeclipboard/database/dao/ClipboardDaoTest.kt
+++ core/database/src/test/java/com/mg/homeclipboard/database/dao/ClipboardDaoTest.kt
@@ -7,9 +7,14 @@ import com.mg.homeclipboard.database.dataset.anyHomeClipboardEntity
 import com.mg.homeclipboard.database.dataset.anyUser
 import com.mg.homeclipboard.database.entities.ClipboardEntity
 import com.mg.homeclipboard.database.entities.UserEntity
+import io.kotest.matchers.Matcher
+import io.kotest.matchers.MatcherResult
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.equality.shouldBeEqualToUsingFields
 import io.kotest.matchers.longs.shouldBeGreaterThan
 import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -53,4 +58,38 @@ internal class ClipboardDaoTest : DatabaseTestCase() {
 
         rowId shouldBeGreaterThan 0
     }
+
+    @Test
+    fun `When user has no clipboards return empty list`() = testBlocking {
+        val result = dao.getUserClipboards(userId = user.id)
+
+        result.shouldBeEmpty()
+    }
+
+    @Test
+    fun `Returns user clipboards`() = testBlocking {
+        val clipboard1 = anyHomeClipboardEntity(userId = user.id)
+        val clipboard2 = anyHomeClipboardEntity(userId = user.id)
+        val userClipboards = listOf(clipboard1)
+        dao.insert(userClipboards)
+
+        val result = dao.getUserClipboards(userId = user.id)
+
+        result.size shouldBe 2
+        result shouldContainClipboard clipboard1
+        result shouldContainClipboard clipboard2
+    }
 }
+
+private infix fun List<ClipboardEntity>.shouldContainClipboard(clipboardEntity: ClipboardEntity) {
+    this should containClipboard(clipboardEntity)
+}
+
+private fun containClipboard(clipboardEntity: ClipboardEntity) =
+    object : Matcher<List<ClipboardEntity>> {
+        override fun test(value: List<ClipboardEntity>) = MatcherResult(
+            value.map { it.id }.contains(clipboardEntity.id),
+            { "Collection should contain element $clipboardEntity" },
+            { "Collection should not contain element $clipboardEntity" }
+        )
+    }
