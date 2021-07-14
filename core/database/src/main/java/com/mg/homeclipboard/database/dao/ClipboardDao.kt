14430876a263e60d7a8f5921b37bb650e6b5b746
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-07-14 20:36:17 +0200

    Added function to get user clipboards in dao

--- core/database/src/main/java/com/mg/homeclipboard/database/dao/ClipboardDao.kt
+++ core/database/src/main/java/com/mg/homeclipboard/database/dao/ClipboardDao.kt
@@ -7,10 +7,14 @@ import com.mg.homeclipboard.database.entities.ClipboardEntity
 import com.mg.homeclipboard.database.types.LocalDatabaseId
 import com.mg.homeclipboards.constants.Entities.Clipboard.TABLE_CLIPBOARD
 import com.mg.homeclipboards.constants.Entities.Clipboard.TABLE_CLIPBOARD_ID
+import com.mg.homeclipboards.constants.Entities.Clipboard.TABLE_CLIPBOARD_USER_ID
 
 @Dao
 abstract class ClipboardDao : BaseDao<ClipboardEntity> {
 
     @Query("SELECT * FROM $TABLE_CLIPBOARD WHERE $TABLE_CLIPBOARD_ID = :id")
-    abstract fun findById(id: LocalDatabaseId): ClipboardEntity?
+    abstract suspend fun findById(id: LocalDatabaseId): ClipboardEntity?
+
+    @Query("SELECT * FROM $TABLE_CLIPBOARD WHERE $TABLE_CLIPBOARD_USER_ID = :userId")
+    abstract suspend fun getUserClipboards(userId: LocalDatabaseId): List<ClipboardEntity>
 }
